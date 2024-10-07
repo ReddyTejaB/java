2a9d8ef31790afc20a6a59ed49f82c6625720f59
ReddyTejaB <bathalareddyteja@gmail.com>
2024-10-07 20:23:36 +0530

    Main file added

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    String name;
+    int age;
+    double weight;
+
+    public Main (String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+        public static void main(String[]args){
+            Main s1 = new Main("Reddy",21,5.6);
+            Main s2 = new Main("Teja",24,5.5);
+            System.out.println(s1.name+" "+s1.age+" "+s1.weight);
+
+
+    }
+}
