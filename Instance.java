d7d0a5cb6992cfe98001b145b70da1991d2402a7
ReddyTejaB <bathalareddyteja@gmail.com>
2024-10-08 07:10:07 +0530

    Instance

--- Instance.java
+++ Instance.java
@@ -1,22 +1,21 @@
 public class Instance {
     //variables
 
-    String name;
     int age;
     double weight;
+    
     //constrcter
 
-    public Instance(String name, int age, double weight) {
-        this.name = name;
+    public Instance(int age, double weight) {
         this.age = age;
         this.weight = weight;
     }
     //methods
 
     public static void main(String[] args) {
-        Instance i1 = new Instance ("Jack",23,76.0);
-        Instance i2 = new Instance ("Spark",24,67.5);
-        System.out.println(i1.name+" "+i1.age+" "+i1.weight);
-        System.out.println(i2.name+" "+i2.age+" "+i2.weight);
+        Instance i1 = new Instance (23,76.0);
+        Instance i2 = new Instance (24,67.5);
+        System.out.println(i1.age+" "+i1.weight);
+        System.out.println(i2.age+" "+i2.weight);
     }
 }
