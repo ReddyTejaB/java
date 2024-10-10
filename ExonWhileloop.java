2b2c5724916838d8ad487ba9fb074ba6d96cce9b
ReddyTejaB <bathalareddyteja@gmail.com>
2024-10-10 09:41:11 +0530

    whileloop

---
+++ ExonWhileloop.java
@@ -0,0 +1,10 @@
+public class ExonWhileloop {
+    public static void main(String[] args) {
+        int i=1,sum=1;
+        while (i<5){
+            sum = sum+i;
+            System.out.println(sum);
+            i++;
+        }
+    }
+}
