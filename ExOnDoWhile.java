12ff4144beff4c2af11ad04a494d17a0e47a3cce
ReddyTejaB <bathalareddyteja@gmail.com>
2024-10-10 09:43:09 +0530

    dowhile

---
+++ ExOnDoWhile.java
@@ -0,0 +1,14 @@
+public class ExOnDoWhile {
+    public static void main(String [] args){
+        int i=1;
+        int sum=0;
+        do {
+            sum = sum + i;
+            System.out.println(sum);
+            i++;
+        }while (i < 5);
+
+            }
+        }
+
+
