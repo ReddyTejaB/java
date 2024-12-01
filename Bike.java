adba53f9e24a692b74cc516a69277196d29c480b
ReddyTejaB <bathalareddyteja@gmail.com>
2024-12-01 15:43:08 +0530

    SealedclassStep3

---
+++ Bike.java
@@ -0,0 +1,9 @@
+package Java18;
+
+    public final class Bike extends Sealdclass{
+        @Override
+        int add(int a, int b) {
+            return a+b;
+        }
+    }
+
---
+++ Car.java
@@ -0,0 +1,8 @@
+package Java18;
+
+public final class Car extends Sealdclass{
+    @Override
+    int add(int a, int b) {
+        return a+b;
+    }
+}
